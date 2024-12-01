984f5123cecd8a978681b9ec5fb438da2801e51b
osvaldo morales <osvaldo.morales08@inacapmail.com>
2024-12-01 07:01:25 -0300

    new changes updates

--- voto_crud/wsgi.py
+++ voto_crud/wsgi.py
@@ -6,7 +6,7 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
+from whitenoise import WhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voto_crud.settings')
 
 application = get_wsgi_application()
+
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
