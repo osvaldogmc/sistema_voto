425a670287e9b50fbd40aae45dc52c658da4d155
osvaldo morales <osvaldo.morales08@inacapmail.com>
2024-12-10 02:22:26 -0300

    the final

--- build.sh
+++ build.sh
@@ -8,11 +8,6 @@ pip install -r requirements.txt
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-delete FROM voto_voto;
-
-delete FROM voto_votante;
-
-delete FROM voto_candidato;
 
 # Apply any outstanding database migrations
 python manage.py migrate
